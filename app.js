4c4c9543860014b990e99ee173888e3289a3251a
notonly.Design <62813511+hiamjoseph@users.noreply.github.com>
2022-02-02 18:18:19 +0100

    more

---
+++ app.js
@@ -0,0 +1,14 @@
+const apple = {
+    name: 'apple',
+    calories: 5,
+    edible: true,
+    howTo: function (step1, step2) {
+        console.log('apples are great');
+    }
+}
+
+console.log(apple);
+
+
+
+
